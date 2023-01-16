72552eb3679bacab675883d27ba44184232c05a6
Till <hummel.till@web.de>
2023-01-17 00:42:38 +0100

    remove unused constants

--- src/main/java/de/theunknownlarry/betterenchantments/Main.java
+++ src/main/java/de/theunknownlarry/betterenchantments/Main.java
@@ -15,11 +15,7 @@ import org.jetbrains.annotations.NotNull;
 @Mod(Main.MODID)
 public class Main
 {
-
 	public static final String MODID = "betterenchantments";
-    public static final String NAME = "BetterEnchantments";
-    public static final String VERSION = "1.0";
-    public static final String MC_VERSIONS = "[1.19.2]";
     public static final CreativeModeTab TAB_BETTERENCHANTMENTS = new CreativeModeTab(MODID) {
         @Override
         public @NotNull ItemStack makeIcon()
